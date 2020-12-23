2d2a0bc89bc22dd2c26931e93470ee52a6879a31
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2020-12-23 10:38:41 -0500

    added styles to the structure of the page

--- components/calculator.jsx
+++ components/calculator.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 export default () => (
   <div className="main-content">
-    <input id ="firstNum" type="text" />
-    <select name="operators" id="operator" >
-        <option value="+">+</option>
-        <option value="-">-</option>
-        <option value="*">*</option>
-        <option value="/">/</option>
+    <input id="firstNum" type="text" />
+    <select name="operators" id="operator">
+      <option value="+">+</option>
+      <option value="-">-</option>
+      <option value="*">*</option>
+      <option value="/">/</option>
     </select>
-    <input id ="secondNum" type="text"/>
+    <input id="secondNum" type="text" />
   </div>
 )
