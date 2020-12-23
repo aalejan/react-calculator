2f38da78e6f97c1bf7fc0cd5309e056e2038e35b
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2020-12-22 22:43:23 -0500

    basic calculator structure created

---
+++ components/calculator.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default () => (
+  <div className="main-content">
+    <input id ="firstNum" type="text" />
+    <select name="operators" id="operator" >
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+    </select>
+    <input id ="secondNum" type="text"/>
+  </div>
+)
