ce7dec5ef6f041474e44bf51fbdf0078ec2f35e9
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2020-12-24 12:28:56 -0500

    answer shows up when I use a span tag

--- components/calculator.jsx
+++ components/calculator.jsx
@@ -1,16 +1,49 @@
+/* eslint-disable radix */
 import React from 'react'
 
+function webCalculator() {
+  let answer
+  const firstNum = Number(document.getElementById('firstNum').value)
+  const secondNum = Number(document.getElementById('secondNum').value)
+
+  const operator = document.getElementById('operator')
+  const operation = operator.options[operator.selectedIndex].value
+
+  switch (operation) {
+    case '+':
+      answer = parseInt(firstNum) + parseInt(secondNum)
+      break
+
+    case '-':
+      answer = parseInt(firstNum) - parseInt(secondNum)
+      break
+
+    case '/':
+      answer = parseInt(firstNum) / parseInt(secondNum)
+      break
+
+    case '*':
+      answer = parseInt(firstNum) * parseInt(secondNum)
+      break
+
+    default:
+      answer = alert('Unknown')
+  }
+  document.getElementById('answer').value = answer
+}
+
 export default () => (
+
   <div className="main-content">
     <input id="firstNum" type="text" />
-    <select name="operators" id="operator">
+    <select name="operations" id="operator">
       <option value="+">+</option>
       <option value="-">-</option>
       <option value="*">*</option>
       <option value="/">/</option>
     </select>
     <input id="secondNum" type="text" />
-    <button type="submit" className="calculate-button">=</button>
-    <input type="text" id="answer" />
+    <button type="submit" className="calculate-button" onClick={webCalculator}>=</button>
+    <input type="text" />
   </div>
 )
